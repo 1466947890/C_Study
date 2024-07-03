a0bf2eefaac902ff61c7af1e86b6a1e92b4e2ec1
韦长佳 <weicj@enca.com.cn>
2024-07-03 13:05:47 +0800

    '队列'

--- 数据结构/队列.c
+++ 数据结构/队列.c
@@ -16,6 +16,7 @@ Node* initQueue()
   return Q;
 }
 
+// 入节点操作
 void enQueue(Node* Q, int data)
 {
   Node* q = Q;
@@ -30,10 +31,59 @@ void enQueue(Node* Q, int data)
   Q->data ++;
 }
 
+int isEmpty(Node* Q)
+{
+  if(Q->data == 0 || Q->next == NULL)
+  {
+    return 1;
+  }else{
+    return 0;
+  }
+}
+
+int deQueue(Node* Q)
+{
+  if(isEmpty(Q))
+  {
+    return -1;
+  }else{
+    Node* node = Q->next;
+    int data = node->data;
+    Q->next = node->next;
+    free(node);
+    Q->data--;
+    return data;
+  }
+}
+
+// 打印节点操作
+void printQueue(Node* Q)
+{
+  Node* node = Q->next;
+  while (node)
+  {
+    printf("%d -> ", node->data);
+    node = node->next;
+  }
+
+  printf("NULL\n");
+}
+
+
+
+
 
 
 int main()
 {
 
-
+  Node* Q = initQueue();
+  enQueue(Q, 1);
+  enQueue(Q, 2);
+  enQueue(Q, 3);
+  enQueue(Q, 4);
+  printQueue(Q);
+  int data = deQueue(Q);
+  printf("data = %d\n", data);
+  printQueue(Q);
 }
